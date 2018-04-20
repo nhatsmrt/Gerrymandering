5840d0e46f234255eb6983f82960800ece336e02
Nhat Pham <hoangnhat123456@live.com>
2018-04-20 11:25:15 -0400

    Fix Daniel EG

--- Source/partisan_bias.py
+++ Source/partisan_bias.py
@@ -22,10 +22,10 @@ def jimmy_efficiency_gap_v2(districts):
 def daniel_efficiency_gap(districts):
     n_districts = districts.shape[0]
     n_seat_A = (districts[:, 0] > districts[:, 1]).astype(np.int64)
-    return (2 * np.sum(districts[:, 0]) - np.sum(n_seat_A * np.sum(districts, axis = 1))) / n_districts - 0.5
+    return (2 * np.sum(districts[:, 0]) - np.sum(n_seat_A * np.sum(districts, axis = 1)) - 0.5 * np.sum(districts)) / n_districts
 
 
 
 
-districts = np.array([[95, 5], [40, 60], [75, 25], [45, 55], [45, 55]])
-print(daniel_efficiency_gap(districts))
+# districts = np.array([[95, 5], [40, 60], [75, 25], [45, 55], [45, 55]])
+# print(daniel_efficiency_gap(districts))
